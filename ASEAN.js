56f94761419fe88ea6546c46ab4940329130e835
Arthur Wang <ArthurWang2000@hotmail.com>
2020-07-11 19:26:20 +1000

    Implemented pseudo hover effect in js

--- ASEAN.js
+++ ASEAN.js
@@ -1,8 +1,35 @@
 // 
 
-const myanmar = document.querySelector('#myanmar');
-const myanmar_text = document.querySelector('#myanmar_text');
+const countries = [
+    {name: "Brunei", description: "Is an Asean Country"},
+    {name: "Cambodia", description: "Is an Asean Country"},
+    {name: "Indonesia", description: "Is an Asean Country"},
+    {name: "Laos", description: "Is an Asean Country"},
+    {name: "Malaysia", description: "Is an Asean Country"},
+    {name: "Myanmar", description: "Is an Asean Country"},
+    {name: "Philippines", description: "Is an Asean Country"},
+    {name: "Singapore", description: "Is an Asean Country"},
+    {name: "Thailand", description: "Is an Asean Country"},
+    {name: "VietNam", description: "Is an Asean Country"}
+];
 
-myanmar.addEventListener("click", () => {
-    myanmar_text.setAttribute("class", ".invisible");
+let country_info = document.querySelector('#info');
+
+// loop through every country id and add hover-like tag where information about the country is displayed 
+countries.forEach((country) => {
+    let lcCountry = country.name.toLowerCase();
+    let country_id = '#' + lcCountry;
+    console.log(country_id);
+    let country_selected = document.querySelector(country_id);
+    country_selected.addEventListener("mouseover", () => {
+        // may replace with innerHTML for styling or something 
+        country_info.innerText = country.name + "\n" + country.description;
+
+    });
+    
+    country_selected.addEventListener("mouseout", () => {
+        country_info.innerText = "";
+    });
+
 });
+
