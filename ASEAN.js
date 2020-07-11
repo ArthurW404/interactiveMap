2eccfe869d4f7bf626db5cc67c5cd73b969f2cce
Arthur Wang <ArthurWang2000@hotmail.com>
2020-07-11 17:49:32 +1000

    Adding map, some styling and js

---
+++ ASEAN.js
@@ -0,0 +1,8 @@
+// 
+
+const myanmar = document.querySelector('#myanmar');
+const myanmar_text = document.querySelector('#myanmar_text');
+
+myanmar.addEventListener("click", () => {
+    myanmar_text.setAttribute("class", ".invisible");
+});
