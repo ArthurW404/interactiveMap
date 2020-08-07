db8f7fda0a762614bae193f7099870b3c4f39341
Arthur Wang <z5257096@ad.unsw.edu.au>
2020-08-07 21:18:18 +1000

    Updated information, need to style it better

--- ASEAN.js
+++ ASEAN.js
@@ -3,43 +3,83 @@
 const countries = [
     {
         name: "Brunei",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 9.2% \n \
+            \u25CA Labour Force Participation - 62.7% \n \
+            \u25CA GDP (2018) - $14 Billion  \n \
+            \u25CA Population (2018) - 0.43 million  \n "
     },
     { 
         name: "Cambodia",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 1.1% \n \
+            \u25CA Labour Force Participation - 84.3% \n \
+            \u25CA GDP (2018) - $25 Billion  \n \
+            \u25CA Population (2018) - 16.25 million  \n "
     },
     { 
         name: "Indonesia",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 5.3% \n \
+            \u25CA Labour Force Participation -  68.3% \n \
+            \u25CA GDP (2018) - $1.04 Trillion  \n \
+            \u25CA Population (2018) - 267.7 million \n "
     },
     { 
         name: "Laos",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 0.6% \n \
+            \u25CA Labour Force Participation - 64.1% \n \
+            \u25CA GDP (2018) - $18 Billion  \n \
+            \u25CA Population (2018) - 7.062 million \n "
     },
     { 
         name: "Malaysia",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 3.2% \n \
+            \u25CA Labour Force Participation - 67.7% \n \
+            \u25CA GDP (2018) - $354 Billion  \n \
+            \u25CA Population (2018) - 31.53 million \n "
     },
     { 
         name: "Myanmar",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 4.0 % \n \
+            \u25CA Labour Force Participation - 62% \n \
+            \u25CA GDP (2018) - $71 Billion  \n \
+            \u25CA Population (2018) - 53.71 million \n "
     },
     { 
         name: "Philippines",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 5.4% \n \
+            \u25CA Labour Force Participation - 60.1% \n \
+            \u25CA GDP (2018) - $331 Billion  \n \
+            \u25CA Population (2018) - 106.7 million \n "
     },
     { 
         name: "Singapore",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 2.9% \n \
+            \u25CA Labour Force Participation - 68% \n \
+            \u25CA GDP (2018) - $364 Billion  \n \
+            \u25CA Population (2018) - 5.639 million \n "
     },
     { 
         name: "Thailand",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 1.1% \n \
+            \u25CA Labour Force Participation - 68.3% \n \
+            \u25CA GDP (2018) - $505 Billion  \n \
+            \u25CA Population (2018) - 69.43 million \n "
     },
     { 
         name: "VietNam",
-        description: "\u25CA Is an Asean Country"
+        description: 
+            "\u25CA Unemployment Rate - 2.2% \n \
+            \u25CA Labour Force Participation - 76.8% \n \
+            \u25CA GDP (2018) - $245 Billion  \n \
+            \u25CA Population (2018) - 95.54 million \n "
     }
 ];
 
